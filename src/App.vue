81d1a34e2d7fb9e559b8ef9cdb059528a420cbc2
madhukar01 <mholla8@gmail.com>
2022-07-04 02:12:06 +0530

    feat(3D): three js sample cube

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,10 @@
     <v-main>
       <nav>
         <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
+        <router-link to="/sample">Sample</router-link>
       </nav>
       <router-view />
+      <div id="mycanvas"></div>
     </v-main>
   </v-app>
 </template>
