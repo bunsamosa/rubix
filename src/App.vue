e22e937996b5c081b67b55d38c97cd3c164af5a8
0xMadhukar <mholla8@gmail.com>
2022-07-10 21:57:36 +0530

    feat(nav): navigation bar

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,23 @@
 
 <template>
   <v-app>
-    <v-main>
-      <nav>
-        <router-link to="/">Home</router-link>
-        <router-link to="/cube">Cube</router-link>
-        <router-link to="/star">Star</router-link>
-        <router-link to="/robot">Robot</router-link>
-      </nav>
-      <router-view />
-    </v-main>
+    <v-app-bar app>
+      <v-app-bar-title>Rubix</v-app-bar-title>
+      <v-tabs align-with-title>
+        <v-tab>
+          <router-link to="/">Home</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/cube">Cube</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/star">Star</router-link>
+        </v-tab>
+        <v-tab>
+          <router-link to="/robot">Robot</router-link>
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
+    <router-view />
   </v-app>
 </template>
