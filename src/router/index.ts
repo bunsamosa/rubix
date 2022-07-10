f899a3415a1c04d8628b37fe1a164223916c4dae
0xMadhukar <mholla8@gmail.com>
2022-07-10 21:46:21 +0530

    feat(robot): robot gltf renderer

--- src/router/index.ts
+++ src/router/index.ts
@@ -20,10 +20,12 @@ const router = createRouter({
     {
       path: "/star",
       name: "star",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/StarView.vue"),
+    },
+    {
+      path: "/robot",
+      name: "robot",
+      component: () => import("../views/RobotView.vue"),
     },
   ],
 });
