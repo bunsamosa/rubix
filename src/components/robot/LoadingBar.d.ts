a53bc84c9bb8d9b1b378be74b2e0cf101aabae62
Dhanush Kanchan <dhanushkanchan@gmail.com>
2022-08-03 13:38:15 +0530

    fix(cleanup): remove maps and samples

--- src/components/robot/LoadingBar.d.ts
+++
@@ -1,5 +0,0 @@
-import { LoadingBar } from "./LoadingBar.js";
-
-export namespace LoadingBar {
-    function onprogress(delta);
-}
--- src/views/RobotView.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-import RobotRenderer from "@/components/robot/RobotRenderer.vue";
-</script>
-
-<template>
-    <RobotRenderer />
-</template>
