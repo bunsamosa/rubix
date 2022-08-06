171686b586df58b62255720c8f740a1541df8763
Dhanush Kanchan <dhanushkanchan@gmail.com>
2022-08-06 16:32:49 +0530

    feat(game): preload assets

--- src/components/city/game.ts
+++ src/components/city/game.ts
@@ -2,7 +2,7 @@ import WEBGL from "three/examples/jsm/capabilities/WebGL";
 import * as THREE from "three";
 
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-import { JoyStick } from "./toon3d";
+import { JoyStick, SFX, Preloader } from "./toon3d";
 
 // game class
 class Game {
@@ -16,115 +16,65 @@ class Game {
     public sun: THREE.DirectionalLight;
     public joystick: JoyStick | any;
     public colliders: Array<THREE.Mesh>;
+    public modes: any;
+    public mode: any;
+    public assetsPath: string;
+    public messages: any;
+    public animationFiles: Array<string>;
+    public loadingManager: THREE.LoadingManager;
 
     constructor(container: HTMLElement) {
 
         // check if webgl is enabled
-        if (WEBGL.isWebGLAvailable() === false) {
-            alert(WEBGL.getWebGLErrorMessage());
-        };
+        if (WEBGL.isWebGLAvailable() === false) alert(WEBGL.getWebGLErrorMessage());
+
+        // game modes for preloader
+        this.modes = Object.freeze({
+            NONE: Symbol("none"),
+            PRELOAD: Symbol("preload"),
+            INITIALISING: Symbol("initialising"),
+            CREATING_LEVEL: Symbol("creating_level"),
+            ACTIVE: Symbol("active"),
+            GAMEOVER: Symbol("gameover")
+        });
+        this.mode = this.modes.NONE;
 
         // game variables
-        const game = this;
         this.player = {} as Player;
         this.animations = new Map();
         this.container = container;
         this.colliders = [];
-        const animFiles = ["walking", "running", "walkback", "turn", "idle"];
-
-        // initialize threejs components
-        // create game clock
         this.clock = new THREE.Clock();
 
-        // setup camera
-        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 5000);
-        this.camera.position.set(112, 100, 600);
+        this.messages = {
+            text: [
+                "Welcome to Rubix",
+                "GOOD LUCK!"
+            ],
+            index: 0
+        }
 
-        // create scene
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color(0xa0a0a0);
-        this.scene.fog = new THREE.Fog(0xa0a0a0, 700, 4000);
-
-        // setup ambient light
-        const ambientLight = new THREE.HemisphereLight(0xffffff, 0x444444);
-        ambientLight.position.set(0, 200, 0);
-        this.scene.add(ambientLight);
-
-        // setup directional light and shadows
-        const shadowSize = 200;
-        const dicrectionalLight = new THREE.DirectionalLight(0xffffff);
-        dicrectionalLight.position.set(0, 200, 100);
-        dicrectionalLight.castShadow = true;
-        dicrectionalLight.shadow.camera.top = shadowSize;
-        dicrectionalLight.shadow.camera.bottom = -shadowSize;
-        dicrectionalLight.shadow.camera.left = -shadowSize;
-        dicrectionalLight.shadow.camera.right = shadowSize;
-        this.sun = dicrectionalLight;
-        this.scene.add(dicrectionalLight);
-
-        // create ground material
-        const ground = new THREE.Mesh(
-            new THREE.PlaneBufferGeometry(10000, 10000),
-            new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
-        );
-        ground.rotation.x = - Math.PI / 2;
-        ground.receiveShadow = true;
-        this.scene.add(ground);
-
-        // setup grid on the ground
-        const grid = new THREE.GridHelper(5000, 40, 0x000000, 0x000000);
-        let material = grid.material as THREE.Material
-        material.opacity = 0.2;
-        material.transparent = true;
-        this.scene.add(grid);
-
-        // load player model
-        const loader = new FBXLoader();
-        loader.load("assets/players/player1.fbx", (object) => {
-            // animations
-            const mixer = new THREE.AnimationMixer(object);
-            game.player.mixer = mixer;
-            game.player.root = mixer.getRoot();
-
-            // name for the object
-            object.name = "player";
-
-            // traverse children and update attributes
-            object.traverse(function (child) {
-                let isMesh = (child as THREE.Mesh).isMesh;
-                if (isMesh) {
-                    child.castShadow = true;
-                    child.receiveShadow = false;
-                }
-            });
-
-            // load textures
-            // const tLoader = new THREE.TextureLoader();
-            // tLoader.load("assets/city/Boy01_normal.jpg", (texture) => {
-            //     object.traverse(function (child) {
-            //         if (child.isMesh) {
-            //             child.material.map = texture;
-            //         }
-            //     });
-            // });
-
-            // wrap player in a 3D object
-            game.player.object = new THREE.Object3D();
-            game.scene.add(game.player.object);
-            game.player.object.add(object);
-            game.animations["idle"] = object.animations[0];
-            game.loadAnimations(loader, animFiles);
+        // asset preloading
+        this.assetsPath = "/assets/city";
+        this.animationFiles = ["walking", "running", "walkback", "turn", "idle"];
+        const game = this;
+        const options = {
+            assets: [
+                `${this.assetsPath}/city.fbx`,
+                `${this.assetsPath}/Volumes/Vault/Dropbox/BITGEM_Products/_smashy_craft_series/city/city/construction/city_tex.tga`
+            ],
+            oncomplete: function () {
+                game.init();
+                game.animate();
+            }
+        };
+        this.animationFiles.forEach(filename => {
+            options.assets.push(`assets/animations/${filename}.fbx`)
         });
+        this.mode = this.modes.PRELOAD;
+        const preloader = new Preloader(options);
 
-        // create renderer and attach to DOM
-        this.renderer = new THREE.WebGLRenderer({ antialias: true });
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.renderer.shadowMap.enabled = true;
-        this.container.appendChild(this.renderer.domElement);
-
-        // add event listener to resize canvas on window resize
-        window.addEventListener("resize", () => { game.onWindowResize(); }, false);
+        // const sfxExt = SFX.supportsAudioType('mp3') ? 'mp3' : 'ogg';
 
         // generic error handler
         window.addEventListener("error", (error) => { console.error(JSON.stringify(error)); });
