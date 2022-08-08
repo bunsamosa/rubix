5bf2caa96d84579edf7655389904e161c373be37
madhukar01 <mholla8@gmail.com>
2022-08-08 22:06:29 +0530

    feat(theatre): pass validation with ethpass

--- src/components/city/CityRenderer.vue
+++ src/components/city/CityRenderer.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { Game } from "./game";
-import VideoStream from "@/views/VideoPlayer.vue";
 
 // fetch container ID from props
 const props = defineProps<{
@@ -15,41 +14,14 @@ const game = new Game(container);
 <template>
     <v-row justify="space-around">
         <v-col cols="auto">
-            <v-dialog transition="dialog-bottom-transition">
-                <template v-slot:activator="{ props }">
-                    <v-btn color="primary" v-bind="props">Theatre</v-btn>
-                </template>
-                <template v-slot:default="{ isActive }">
-                    <v-card>
-                        <v-toolbar color="primary">Rubix Live Stream</v-toolbar>
-                        <v-container>
-                            <VideoStream />
-                        </v-container>
-                        <v-card-actions class="justify-end">
-                            <v-btn text @click="isActive.value = false">Close</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </template>
-            </v-dialog>
+            <router-link to="/stream" target="_blank">
+                <v-btn color="primary">Host Event</v-btn>
+            </router-link>
         </v-col>
-
-        <!-- <v-col cols="auto">
-            <v-dialog transition="dialog-top-transition">
-                <template v-slot:activator="{ props }">
-                    <v-btn color="primary" v-bind="props">From the top</v-btn>
-                </template>
-                <template v-slot:default="{ isActive }">
-                    <v-card>
-                        <v-toolbar color="primary">Opening from the top</v-toolbar>
-                        <v-card-text>
-                            <div class="text-h2 pa-12">Hello world!</div>
-                        </v-card-text>
-                        <v-card-actions class="justify-end">
-                            <v-btn text @click="isActive.value = false">Close</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </template>
-            </v-dialog>
-        </v-col> -->
+        <v-col cols="auto">
+            <router-link to="/validate" target="_blank">
+                <v-btn color="primary">Theatre</v-btn>
+            </router-link>
+        </v-col>
     </v-row>
 </template>
